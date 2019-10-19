c9b4cadadfeaf6d527d294e216bd77bddc100f63
kesavanbala <kesavanbk2@gmail.com>
2019-10-19 20:43:52 +0530

    FirstPro

---
+++ NewEclipse/src/First.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class First {
+
+	public static void main(String[] args) {
+				String r="";
+				int c=0;
+				String st="tamil nadu is beautiful city";
+				for(int i=0;i<st.length();i++)
+				{
+					int count = 0;
+					for(int j=i+1;j<st.length();j++)
+					{
+						if(Character.toString(st.charAt(i)).equals(Character.toString(st.charAt(j))))
+								{
+								count++;
+								}
+					}
+					if(count==0)
+					{
+						c++;
+					}
+					if(c==2)
+					{
+						System.out.println(st.charAt(i));
+						
+					}
+				}
+	
+	
+	
+	}
+}
